12de0bd39aeb47571c51194e22305c7c8f977ea5
scaiciia <scaiciia@gmail.com>
2026-04-24 21:26:27 -0300

    modified:   TP4 - TypeScript/src/main.ts 	modified:   TP4 - TypeScript/src/pages/auth/login/login.ts

--- TP4 - TypeScript/src/main.ts
+++ TP4 - TypeScript/src/main.ts
@@ -1,13 +1,24 @@
 import type { IUser } from "./types/IUser";
+import { logout } from "./utils/auth";
 import { getUSer } from "./utils/localStorage";
 import { navigate } from "./utils/navigate";
 
+function isIndexOrLogin(path: string): boolean {
+    return path === "/" || path === "/src/pages/auth/login/login.html"
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const user: string | null = getUSer();
+    const path:string = window.location.pathname
     if (!user) {
         console.log("No hay usuario.")
-        navigate("/src/pages/auth/login/login.html");
-    } else {
+        if (!path.includes("/auth/")) {
+            navigate("/src/pages/auth/login/login.html");
+        }
+        return;
+    }
+
+    try {
         const parseUser: IUser = JSON.parse(user);
         if (!parseUser.loggedIn) {
             console.log("El usuario no esta loggeado")
@@ -15,15 +26,18 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         console.log(window.location.pathname)
-        if ((window.location.pathname.includes("/client/") && parseUser.role === "admin") || (window.location.pathname === "/" && parseUser.role === "admin")) {
+        if ((path.includes("/client/") && parseUser.role === "admin") || (isIndexOrLogin(path) && parseUser.role === "admin")) {
             console.log("El usuario es admin");
             navigate("/src/pages/admin/home/home.html");
         }
 
-        if ((window.location.pathname.includes("/admin/") && parseUser.role !== "admin") || (window.location.pathname === "/" && parseUser.role !== "admin")) {
+        if ((path.includes("/admin/") && parseUser.role !== "admin") || (isIndexOrLogin(path) && parseUser.role !== "admin")) {
             console.log("El usuario no es admin.");
             navigate("/src/pages/client/home/home.html");
         }
-
+    
+    } catch(error) {
+        console.error("Error en la sesión, limpiando datos...");
+        logout();
     }
 })
--- TP4 - TypeScript/src/pages/auth/login/login.ts
+++ TP4 - TypeScript/src/pages/auth/login/login.ts
@@ -15,34 +15,37 @@ if (form && inputEmail && inputPassword && selectRol) {
     if (valueEmail.trim() == "") {
       alert("El campo email debe tener un correo.");
       console.error("El campo de email esta vacio.")
+      return;
     }
 
     const valuePassword = inputPassword.value;
     if (valuePassword == "") {
       alert("Debe ingresar una contraseña.");
       console.error("El campo de password esta vacio.");
+      return;
     }
 
     const valueRol = selectRol.value as Rol;
     if (!valueRol) {
       alert("Debe seleccionar un rol.");
       console.error("El campo rol se esta vacio.");
+      return;
     }
 
     const usuarios: IUser[] = getUsers();
     if (usuarios.length == 0) {
       alert("No hay usuarios registrados en el sistema.");
       console.error("No hay usuarios registrados");
+      return;
     }
 
-    if (usuarios.find(u => u.email == valueEmail && u.password == valuePassword)) {
+    if (!usuarios.find(u => u.email == valueEmail && u.password == valuePassword)) {
+      alert("El email y/o contraseña son incorrectos");
+      console.error("Usuario y/o contraseña son incorrectos.");
+      return;
+    }
 
-      if (valueRol === "admin") {
-        navigate("/src/pages/admin/home/home.html");
-      } else if (valueRol === "client") {
-        navigate("/src/pages/client/home/home.html");
-      }
-
+    try {
       const user: IUser = {
         email: valueEmail,
         password: valuePassword,
@@ -52,9 +55,14 @@ if (form && inputEmail && inputPassword && selectRol) {
 
       const parseUser = JSON.stringify(user);
       localStorage.setItem("userData", parseUser);
-    } else {
-      alert("El email y/o contraseña son incorrectos");
-      console.log("Usuario y/o contraseña son incorrectos.");
+
+      if (valueRol === "admin") {
+        navigate("/src/pages/admin/home/home.html");
+      } else if (valueRol === "client") {
+        navigate("/src/pages/client/home/home.html");
+      }
+    } catch(error) {
+      console.error("Error al intentar loggear. Intentelo nuevamente.");
     }
 
   });
