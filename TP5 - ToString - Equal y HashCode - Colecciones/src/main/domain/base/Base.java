c40956dc2b6dadc45f02a81112b1586aff340c88
smassello <scaiciia@gmail.com>
2026-05-26 22:49:01 -0300

    mod: toString, equals y hashCode de Base

--- TP5 - ToString - Equal y HashCode - Colecciones/src/main/domain/base/Base.java
+++ TP5 - ToString - Equal y HashCode - Colecciones/src/main/domain/base/Base.java
@@ -1,8 +1,9 @@
 package main.domain.base;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
-public class Base {
+public abstract class Base {
 
     private Long id;
     private boolean eliminado;
@@ -36,6 +37,30 @@ public class Base {
 
     public void setCreatedAt(LocalDate createdAt) {
         this.createdAt = createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Base base = (Base) o;
+        return Objects.equals(id, base.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id + ", eliminado=" + eliminado + ", createdAt=" + createdAt;
     }
 
 }
